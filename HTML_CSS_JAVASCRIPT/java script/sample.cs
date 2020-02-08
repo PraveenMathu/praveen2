5d68d028fd210a749d5379690ce8e3a28d9285c5
PraveenMathu <60771232+PraveenMathu@users.noreply.github.com>
2020-02-08 11:41:52 +0530

    Add files via upload

---
+++ HTML_CSS_JAVASCRIPT/java script/sample.cs
@@ -0,0 +1,22 @@
+using system;
+
+namespace csharpFundamentals
+{
+ public class Sample 
+{
+ public static voids Main(srtring{} args)
+{
+int empage;
+string empName;
+ 
+Console.writeLine("Enter employee name\n");
+empName = Console.Readline( );
+Console.WriteLine("Enter emoployee age\n");
+empAge = int.parse(Console.readLine());
+
+}
+
+}
+
+
+}
---
+++ HTML_CSS_JAVASCRIPT/java script/sample2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
+  float a, b,c;
+            Console.WriteLine("enter two numbers");
+            a = Console.Read();
+            b = Console.Read();
+            c = a * b;
+            Console.WriteLine("product of two numbers");
+            c = Console.Read();
